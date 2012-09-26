59c0b031ee4a230d3577e0dce3eb5b93aaabd95b
EmanuelCadems <emanuel_cadems@hotmail.com>
2012-09-26 04:11:42 -0300

    creación de la aplicación nontification of holiday

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clase4::Application.config.secret_token = '4d502bb39522a245ad644053a19c28bf0b2247dd481d7439b62ee0cdec9b0686372534b28cd25a1f83b5ae9cfc59b7f535763a184c1ab58eee4a31f4f8f6ab71'
