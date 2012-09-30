e8cd54e3a032e192d71051729927145eaf396f6e
EmanuelCadems <emanuel_cadems@hotmail.com>
2012-09-30 01:33:06 -0300

    emai

---
+++ app/mailers/newsletter_mailer.rb
@@ -0,0 +1,14 @@
+class NewsletterMailer < ActionMailer::Base
+  default from: "emanuel.cadems@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.newsletter_mailer.weekly.subject
+  #
+  def weekly(email)
+    @greeting = "Hi"
+
+    mail to: "to@example.org", subject: "Notificación de Feriado desde EmanuelCadems"
+  end
+end
